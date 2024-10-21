aa27150dd66b3fa250c0a29d1cc2f72005769c4e
zhoujin <3183170956@qq.com>
2024-10-21 21:54:18 +0800

    RSA加解密

--- RSADemo/src/main/java/com/z/Main.java
+++ RSADemo/src/main/java/com/z/Main.java
@@ -1,7 +1,25 @@
 package com.z;
 
+import com.z.utils.RSAUtils;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //生成种子
+        String keySeed = "RSA_KEY_SEED_" + new Date().getTime();
+        //生成公钥和私钥
+        Map<String, Key> keyMap = RSAUtils.setKeyPair(keySeed);
+        System.out.println(keyMap.toString());
+        String content = "this is my test text,Please encrypt me!";
+        System.out.println("原文:" + content);
+        //公钥加密
+        String encrypt = RSAUtils.encryptWithPublicKey(content, keyMap.get(RSAUtils.PUBLIC_KEY));
+        System.out.println("RSA密文:" + encrypt);
+        //私钥解密
+        String decrypt = RSAUtils.decryptWithPrivateKey(encrypt, keyMap.get(RSAUtils.PRIMARY_KEY));
+        System.out.println("RSA明文:" + decrypt);
     }
 }
--- RSADemo/src/main/java/com/z/utils/RSAUtils.java
+++ RSADemo/src/main/java/com/z/utils/RSAUtils.java
@@ -1,32 +1,74 @@
 package com.z.utils;
 
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
 import java.security.*;
 import java.util.Base64;
 import java.util.HashMap;
 import java.util.Map;
 
 public class RSAUtils {
-    static final String PUBLIC_KEY = "public_key";
-    static final String PRIMARY_KEY = "primary_key";
+    public static final String PUBLIC_KEY = "public_key";
+    public static final String PRIMARY_KEY = "primary_key";
 
     /**
-     * 生成RSA密钥对(base64格式)
+     * 生成RSA密钥对
      *
      * @return
      */
-    public static Map<String, String> setKeyPair() {
+    public static Map<String, Key> setKeyPair(String keySeed) {
         try {
             KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
             //指定密钥长度为2048
-            keyPairGenerator.initialize(2048);
+            keyPairGenerator.initialize(2048, new SecureRandom(keySeed.getBytes()));
             KeyPair keyPair = keyPairGenerator.generateKeyPair();
             PrivateKey aPrivate = keyPair.getPrivate();
             PublicKey aPublic = keyPair.getPublic();
-            Map<String, String> keyMap = new HashMap<>();
-            keyMap.put(PUBLIC_KEY, Base64.getEncoder().encodeToString(aPublic.getEncoded()));
-            keyMap.put(PRIMARY_KEY, Base64.getEncoder().encodeToString(aPrivate.getEncoded()));
+            Map<String, Key> keyMap = new HashMap<>();
+            keyMap.put(PUBLIC_KEY, aPublic);
+            keyMap.put(PRIMARY_KEY, aPrivate);
             return keyMap;
         } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * 公钥加密
+     *
+     * @param input
+     * @param pubKey
+     * @return 返回base64编码的密文
+     */
+    public static String encryptWithPublicKey(String input, Key pubKey) {
+        try {
+            Cipher cipher = Cipher.getInstance("RSA");
+            cipher.init(Cipher.PUBLIC_KEY, pubKey);
+            byte[] bytes = cipher.doFinal(input.getBytes());
+            return Base64.getUrlEncoder().encodeToString(bytes);
+        } catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException | IllegalBlockSizeException |
+                 BadPaddingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * 私钥解密
+     *
+     * @param input  输入base64编码的密文
+     * @param priKey
+     * @return
+     */
+    public static String decryptWithPrivateKey(String input, Key priKey) {
+        try {
+            Cipher cipher = Cipher.getInstance("RSA");
+            cipher.init(Cipher.PRIVATE_KEY, priKey);
+            byte[] bytes = cipher.doFinal(Base64.getUrlDecoder().decode(input));
+            return new String(bytes);
+        } catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException | IllegalBlockSizeException |
+                 BadPaddingException e) {
             throw new RuntimeException(e);
         }
     }
